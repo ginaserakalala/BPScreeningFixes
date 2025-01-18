255292eef9a6e3c7e41122bf332bece9cc6833b6
ginax <ginaserakalala@gmail.com>
2025-01-18 19:57:11 +0200

    Dark mode switch js implementation

--- js/script.js
+++ js/script.js
@@ -37,20 +37,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // *** Login handler ***
   document.getElementById('loginForm').addEventListener('submit',
-    async function(event){
-            event.preventDefault();
+    async function(event) {
+        event.preventDefault();
 
-            const identityNumber = document.getElementById("identityNumber").value.trim();
-            const password = document.getElementById("password").value.trim();
-            // http://localhost:8081/api/auth/signin
-            //https://bp-prod-app-a15e414be88d.herokuapp.com/api/auth/signin
+        const identityNumber = document.getElementById("identityNumber").value.trim();
+        const password = document.getElementById("password").value.trim();
+        // http://localhost:8081/api/auth/signin
+        //https://bp-prod-app-a15e414be88d.herokuapp.com/api/auth/signin
 
-            // Check credentials for the single allowed user
-            // if (username === "HW001" && password === "HW1") {
-            //     window.location.href = "/pages/dashboard.html"; // Redirect to dashboard
-            // } else {
-            //     document.getElementById("loginError").style.display = "block"; // Show error
-            // }
+        // Check credentials for the single allowed user
+        // if (username === "HW001" && password === "HW1") {
+        //     window.location.href = "/pages/dashboard.html"; // Redirect to dashboard
+        // } else {
+        //     document.getElementById("loginError").style.display = "block"; // Show error
+        // }
         try {
             const response = await fetch('https://bp-prod-app-a15e414be88d.herokuapp.com/api/auth/signin',
                 {
@@ -58,35 +58,42 @@ document.addEventListener('DOMContentLoaded', () => {
                     headers: {
                         'Content-Type': 'application/json',
                     },
-                    body: JSON.stringify({ identityNumber, password }),
+                    body: JSON.stringify({identityNumber, password}),
                 }
             );
             if (response.ok) {
-                try {
-                    const data = await response.json();
-                    console.log(data.message); //Handle successful login
-                    window.location.href = "/pages/dashboard.html";
-                } catch (error) {
-                    console.error('Error parsing response as JSON: ', error);
-                    document.getElementById('loginError').textContent = 'An error occurred. Please try again.';
+                const contentType = response.headers.get('Content-Type');
+                if (contentType && contentType.includes('application/json')) {
+                    try {
+                        const data = await response.json();
+                        console.log(data.message); //Handle successful login
+                        window.location.href = "/pages/dashboard.html";
+                    } catch (error) {
+                        console.error('Error parsing response as JSON: ', error);
+                        document.getElementById('loginError').textContent = 'An error occurred. Please try again.';
+                    }
+                } else {
+                    document.getElementById("loginError").style.display = "block"; // Show error
                 }
-            } else if (response.status === 500) {
-                console.error('Internal Server Error');
-                document.getElementById('loginError').textContent = 'An internal server error occurred. Please try again later.';
             } else {
-                try {
-                    const errorData = await response.json();
-                    document.getElementById('loginError').textContent = errorData.message || 'Invalid credentials';
-                } catch (error) {
-                    console.error('Error parsing error response as JSON: ', error);
-                    document.getElementById('loginError').textContent = 'An error occurred. Please try again.';
+                if (response.status === 500) {
+                    console.error('Internal Server Error');
+                    document.getElementById('loginError').textContent = 'An internal server error occurred. Please try again later.';
+                } else {
+                    try {
+                        const errorData = await response.json();
+                        document.getElementById('loginError').textContent = errorData.message || 'Invalid credentials';
+                    } catch (error) {
+                        console.error('Error parsing error response as JSON: ', error);
+                        document.getElementById('loginError').textContent = 'An error occurred. Please try again.';
+                    }
                 }
             }
         } catch (error) {
             console.error('Error logging in: ', error);
             document.getElementById('loginError').textContent = 'An error occurred. Please try again.';
         }
-        });
+    });
     // Check if elements exist before adding event listeners
     const modalOkButton = document.getElementById('modal-ok-btn');
     const modalCancelButton = document.getElementById('modal-cancel-btn');
